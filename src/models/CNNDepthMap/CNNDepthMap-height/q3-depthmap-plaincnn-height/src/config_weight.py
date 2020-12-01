aeebdd114a801dc20ba356ca564cb28eae306752
Markus Hinsche <m.hinsche@gmail.com>
2020-12-01 18:04:42 +0100

    Train better weight model (by using LR find and 1cycle) (#169)
    https://dev.azure.com/cgmorg/ChildGrowthMonitor/_workitems/edit/146
    
    **Motivation:** our weight model was not very good, so we want to improve the accuracy.
    
    **Solution:** 
    - use LR find to find a good learning rate hyperparameter
    - use 1cycle learning schedule to train
    
    We found a promising model for weight! 🎉
    
    **Changes:**
    - We did not create a new experiment dir for weight, because only the config changed (this creates less code duplication)

---
+++ src/models/CNNDepthMap/CNNDepthMap-height/q3-depthmap-plaincnn-height/src/config_weight.py
@@ -0,0 +1,28 @@
+class dotdict(dict):
+    """dot.notation access to dictionary attributes"""
+    __getattr__ = dict.get
+    __setattr__ = dict.__setitem__
+    __delattr__ = dict.__delitem__
+
+
+DATASET_MODE_DOWNLOAD = "dataset_mode_download"
+DATASET_MODE_MOUNT = "dataset_mode_mount"
+
+CONFIG = dotdict(dict(
+    DATASET_MODE=DATASET_MODE_DOWNLOAD,
+    DATASET_NAME="anon-depthmap-95k",
+    DATASET_NAME_LOCAL="anon-depthmap-mini",
+    SPLIT_SEED=0,
+    IMAGE_TARGET_HEIGHT=240,
+    IMAGE_TARGET_WIDTH=180,
+    EPOCHS=1000,
+    BATCH_SIZE=256,
+    SHUFFLE_BUFFER_SIZE=2560,
+    NORMALIZATION_VALUE=7.5,
+    LEARNING_RATE=0.0003,
+    USE_ONE_CYCLE=True,
+    USE_DROPOUT=True,
+
+    # Parameters for dataset generation.
+    TARGET_INDEXES=[1],  # 0 is height, 1 is weight.
+))
